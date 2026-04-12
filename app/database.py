1c2ff6632ee6deb497c37d6a5dea57f408ed1a9c
kontex148 <kontex148@gmail.com>
2026-04-13 01:58:48 +0300

    Updated deploy

--- app/database.py
+++ app/database.py
@@ -1,9 +1,11 @@
 import os
+import urllib.parse
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+# Automatically encode special characters like $
+DB_PASSWORD = urllib.parse.quote_plus(os.getenv("DB_PASSWORD", ""))
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME")
@@ -15,7 +17,6 @@ DATABASE_URL = (
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
